f62db6445a3b7175258c31ea228b012910fddbfd
AleksBartov <aleksandrbartov@yandex.ru>
2020-10-09 01:14:44 +0300

    scss used

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,6 @@ module.exports = {
         theme_color: `#596275`,
       },
     },
+    `gatsby-plugin-sass`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
+import { title } from './index.module.scss'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className={title}>Hello world!</div>
 }
