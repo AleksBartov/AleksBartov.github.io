dd7c166f9d51eda95ffe93ed624c19e4fbd53577
AleksBartov <aleksandrbartov@yandex.ru>
2020-10-10 01:10:36 +0300

    I have finished doing first screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,36 @@
-import React from "react"
-import { title } from './index.module.scss'
+import React, { useState, useEffect } from "react"
+import styles from './index.module.scss'
+
 
 export default function Home() {
-  return <div className={title}>Hello world!</div>
+
+  const [f, setFirst] = useState();
+  const [s, setSecond] = useState();
+
+  useEffect(()=>{
+
+    let first = 0, second = 0;
+
+    const start = setInterval(() => {
+      setFirst(first);
+      setSecond(second);
+
+      first+= .05;
+      second+= .1;
+      if ( first > 30 ) clearInterval(start);
+    }, 10);
+  }, [])
+
+
+  return (<>
+            <div className={styles.box}>
+              <p className={styles.text}>И Слово стало плотию, и обитало с нами, полное благодати и истины; и мы видели славу Его, славу, как Единородного от Отца.</p>
+              <p className={styles.sourse}>( Евангелие от Иоанна. глава 1 стих 14 )</p>
+            </div>
+            <div 
+              className={styles.light}
+              style={{ background: `radial-gradient(circle at 50% 50%, transparent ${f}%, rgba(0,0,0,.95) ${s}%)` }}
+            >
+            </div>
+          </>)
 }
